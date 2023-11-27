917e898bf78e83c591dd6ce96334fe58b1a36691
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2023-11-26 19:02:07 -0500

    Learning anonymous subs, closures.

--- anonsub_close.pl
+++ anonsub_close.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/env perl
 
 # Testing anonymous data structures, subroutines
+# <https://www.perl.com/pub/2002/05/29/closure.html/>
+
+# The subroutine reference below forms a *closure*, enclosing 
+# the lexical variables it refers to. 
 
 sub make_hello_printer { 
-    return sub { print "Hello, world!\n"; }
+    my $message = "Hello, world!\n";
+    return sub { print $message; }
 }
 
 my $print_hello = make_hello_printer();
